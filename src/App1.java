4c63a9edc7cacf65d3cf1df152e78295c2953bc9
anu-cvr <g.annapurna@cvr.ac.in>
2026-03-20 14:08:45 +0530

    updateSRC

---
+++ src/App1.java
@@ -0,0 +1,12 @@
+public class App1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jenkins CI!");
+        System.out.println("This is a simple Java program running in a pipeline.");
+        
+        // Example logic
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
